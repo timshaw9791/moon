c45ddd1a115caedc9c803ba4a021e081569e30be
zSubmit <2951721937@qq.com>
2022-03-12 11:16:30 +0800

    增加一个组件

---
+++ src/components/ZxdComp/Route.vue
@@ -0,0 +1,25 @@
+<template>
+  <template v-for="(item, index) in props.slotNumber" :key="index">
+    <div
+      v-if="props.Page == `page${item}` ? true : false"
+      style="width: 100%; height: 100%"
+    >
+      <slot :name="`page${item}`"></slot>
+    </div>
+  </template>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  slotNumber: {
+    type: Number,
+    default: 2,
+  },
+  Page: {
+    type: String,
+    default: 'page1',
+  },
+})
+</script>
+
+<style scoped></style>
